bdfdeab79fb9f0717fa908e6206917926972d64b
kumarabhimanyu941 <kumarabhimanyu941@gmail.com>
2021-01-28 11:02:33 +0530

    added OrdersPage to the framework

---
+++ src/main/java/com/qa/phpTravels/Pages/OrdersPage.java
@@ -0,0 +1,13 @@
+package com.qa.phpTravels.Pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class OrdersPage {
+
+	WebDriver driver;
+
+	public OrdersPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+}

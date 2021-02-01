998d1cde5774f03b5c24d0ae47f631de6cbe8235
kumarabhimanyu941 <kumarabhimanyu941@gmail.com>
2021-02-01 09:52:42 +0530

    this commit is to test new branch

--- src/main/java/com/qa/phpTravels/Pages/OrdersPage.java
+++ src/main/java/com/qa/phpTravels/Pages/OrdersPage.java
@@ -1,13 +1,18 @@
 package com.qa.phpTravels.Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class OrdersPage {
 
 	WebDriver driver;
 
+	By singIn = By.xpath("this a random test locator");
+	
 	public OrdersPage(WebDriver driver) {
 		this.driver = driver;
 	}
 
+	
+	
 }

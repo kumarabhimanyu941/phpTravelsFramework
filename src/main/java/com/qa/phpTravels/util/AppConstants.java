1f854f4d5f0a03a08784fb2e364253d87eb1e27a
kumarabhimanyu941 <kumarabhimanyu941@gmail.com>
2021-02-10 11:26:57 +0530

    added JDBC connection util

--- src/main/java/com/qa/phpTravels/util/AppConstants.java
+++ src/main/java/com/qa/phpTravels/util/AppConstants.java
@@ -8,5 +8,13 @@ public class AppConstants {
 	public static final String Home_Page_Message = "Login This page is restricted";
 	public static final String Register_Page_Message = "Create an account with us . . .";
 	public static final String Register_Page_Sheet_Name = "register";
-
+	
+	
+	//mySQL DB details
+	
+	public final static String mysqldriver ="com.mysql.jdbc.Driver";
+	public final static String mysqlusername="root";
+	public final static String mysqlpassword="root";
+	public final static String mysqlurl="jdbc:mysql://localhost:3306/DBname";
+	public final static String mysqlquery="select * from purl_attributes where token=''";
 }
